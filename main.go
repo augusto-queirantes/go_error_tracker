aa7323367d32522e5555ed6f3c783510e56faeec
Augusto Queirantes <augusto.queirantes@gmail.com>
2023-03-21 11:58:19 -0300

    Add server

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type Error struct {
+    Name string `json:"name"`
+    StackTrace string `json:"stack_trace"`
+}
+
+func ErrorIndex(response_writter http.ResponseWriter, r *http.Request) {
+    error := Error {
+        Name: "Application Error",
+        StackTrace: "You're application error stack_trace",
+    }
+
+    json.NewEncoder(response_writter).Encode(error)
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+    http.HandleFunc("/", ErrorIndex)
+
+    log.Fatal(http.ListenAndServe(":8080", nil))
 }
